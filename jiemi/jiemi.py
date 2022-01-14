6c850b86c9a3db2a923a61f0376c8d3ed1a2c716
Mr_Solander <729022398@qq.com>
2022-01-14 16:17:25 +0800

    tzg commit

---
+++ jiemi/jiemi.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+# coding=utf-8
+
+# from Crypto.Cipher import AES
+from binascii import b2a_hex, a2b_hex
+import codecs
+
+import base64
+from Crypto.Cipher import AES
+
+def pkcs7padding(text):
+    bs = AES.block_size  # 16
+    length = len(text)
+    bytes_length = len(bytes(text, encoding='utf-8'))
+    # tips：utf-8编码时，英文占1个byte，而中文占3个byte
+    padding_size = length if(bytes_length == length) else bytes_length
+    padding = bs - padding_size % bs
+    # tips：chr(padding)看与其它语言的约定，有的会使用'\0'
+    padding_text = chr(padding) * padding
+    return text + padding_text
+def pkcs7unpadding(text):
+    length = len(text)
+    unpadding = ord(text[length-1])
+    return text[0:length-unpadding]
+def encrypt(key, content):
+    """
+    AES加密
+    key,iv使用同一个
+    模式cbc
+    填充pkcs7
+    :param key: 密钥
+    :param content: 加密内容
+    :return:
+    """
+    key_bytes = bytes(key, encoding='utf-8')
+    iv = key_bytes
+    cipher = AES.new(key_bytes, AES.MODE_CBC, iv)
+    # 处理明文
+    content_padding = pkcs7padding(content)
+    # 加密
+    encrypt_bytes = cipher.encrypt(bytes(content_padding, encoding='utf-8'))
+    # 重新编码
+    result = str(base64.b64encode(encrypt_bytes), encoding='utf-8')
+    return result
+
+def decrypt(key, content):
+    """
+    AES解密
+     key,iv使用同一个
+    模式cbc
+    去填充pkcs7
+    :param key:
+    :param content:
+    :return:
+    """
+    key_bytes = bytes(key, encoding='utf-8')
+    iv = key_bytes
+    cipher = AES.new(key_bytes, AES.MODE_CBC, iv)
+    # base64解码
+    encrypt_bytes = base64.b64decode(content)
+    # 解密
+    decrypt_bytes = cipher.decrypt(encrypt_bytes)
+    # 重新编码
+    result = str(decrypt_bytes, encoding='utf-8')
+    # 去除填充内容
+    result = pkcs7unpadding(result)
+    return result
